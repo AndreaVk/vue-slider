73faac6a096f5d2ac959943bfdfa0ecdaa8b6807
AndreaVk <vico.ac1996@gmail.com>
2021-11-27 17:28:48 +0100

    prime parti

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const app = new Vue({
+    el: '#root',
+    data:{
+        images:[
+            'img/metin2.jpg',  
+            'img/pg.jpeg', 
+            'img/sura1.jpeg', 
+            'img/sura.jpg', 
+            'img/ninja.jpg'  
+        ]
+    },
+    methods: {
+
+    }
+
+});
+
+
