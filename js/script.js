000e47abfa284473aa7b79db2e4b4218144c25ae
AndreaVk <vico.ac1996@gmail.com>
2021-11-29 17:49:27 +0100

    esercizio fine

--- js/script.js
+++ js/script.js
@@ -2,11 +2,31 @@ const app = new Vue({
     el: '#root',
     data:{
         images:[
-            'img/metin2.jpg',  
-            'img/pg.jpeg', 
-            'img/sura1.jpeg', 
-            'img/sura.jpg', 
-            'img/ninja.jpg'  
+        {
+            titolo: 'Metin2',
+            text:'Un gioco orientale MMORPG',
+            image:'img/metin2.jpg'
+        },  
+        {
+            titolo: 'Un gioco Interattivo',
+            text:'Vieni a scoprire il fantastico mondo open space di Metin2',
+            image:'img/pg.jpeg'
+        }, 
+        {
+            titolo:'Scopri i nuovi equipaggiamenti',
+            text:'Iscriviti ora e vieni a vivere questa nuova avventura con migliaia di giocatori',
+            image:'img/sura1.jpeg'
+        }, 
+        {
+            titolo:'Nuovi dungeon',
+            text:'Con il nuovo update sono stati aggiunti nuovi e pericolosi dungeon tutti da scoprire',
+            image:'img/sura.jpg'
+        }, 
+        {
+            titolo:'22/12/18 uscita del nuovo server',
+            text:'Con questa apertura del nuovo server, potrai riniziare alla pari con tutti gli altri giocatori',
+            image:'img/ninja.jpg'
+        }  
         ],
         currentImg: 0
     },
